02096b9a1e3cfbb39f5e4a9ae9a4b48db821df8f
Clinton Crick <clint.crick@gmail.com>
2015-02-13 13:05:04 -0500

    Updated screens, config comments;
    Screens now get generated as needed, not all at once upon load.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 var config = {
     name: 'Prello',
-    version: '1.0',
+    version: '1.1',
     cache: false,
 
     //API SETTINGS;
-    API_KEY:    '',
-    API_SECRET:  '',
-    API_RETURN_URL:  'http://clintoncrick.com/prello/',
+    API_KEY:    '',        //Trello API Key;
+    API_SECRET:  '',       //Trello API Secret (though not actually needed for this);
+    API_RETURN_URL:  'http://clintoncrick.com/prello/',    //URL to any page which can take hash value and direct to this app;
 };
 this.exports = config;
