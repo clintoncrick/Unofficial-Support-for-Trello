afe8155d524241f4301a2b3c04556d322d471938
Clinton Crick <clint.crick@gmail.com>
2015-04-26 20:45:56 -0400

    Name change; bug fix; card improvement;
    Changing name from "Prello" to "Unofficial Support for Trello" due to request from Trello; fixes one bug which prevented archiving multiple cards per instance; improves upon card UX by making card scrollable, which helps cards with long names and/or descriptions;

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ var config = {
     cache: false,
 
     //API SETTINGS;
-    API_KEY:    '259487aa69671ac9aafc5f8672ffe0f7',        //Trello API Key;
+    API_KEY:    '',        //Trello API Key;
     API_SECRET:  '', //Trello API Secret (though not actually needed for this);
     API_RETURN_URL:  'http://clintoncrick.com/prello/',    //URL to any page which can take hash value and direct to this app;
 };
