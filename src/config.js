15054cb8fccf6841492e0c150aa1f2902f841411
Clinton Crick <clint.crick@gmail.com>
2015-12-17 16:24:54 -0500

    Removing API_KEY, adding a note;
    Had kept API_KEY out of the repo. Adding a comment to clarify the intent.

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ var config = {
     
     //API SETTINGS;
     API_URL:    'https://trello.com/1',                    //Trello API URL (moving here from Trello class);
-    API_KEY:    '259487aa69671ac9aafc5f8672ffe0f7',        //Trello API Key;
+    API_KEY:    '',        //Trello API Key- to be replaced by app owner's personal API_KEY. Intending to keep API_KEY out of repo.
     API_SECRET:  '', //Trello API Secret (though not actually needed for this);
     API_RETURN_URL:  'http://clintoncrick.com/prello/',    //URL to any page which can take hash value and direct to this app;
 };
